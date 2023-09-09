96ad56ad1014d718f2f283de5e120921a933aa37
Admin <vinayakkadlag27@gmail.com>
2023-09-09 12:50:30 +0530

    All Pagea

---
+++ CustomerWebApp/Controllers/CustomersController.cs
@@ -0,0 +1,182 @@
+﻿using CustomerWebApp.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CustomerWebApp.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        public IEnumerable<Customer> GetCustomers()
+        {
+            List<Customer> CustomerList = null;
+            try
+            {
+                var getResult = consumeAPI_GET("https://getinvoices.azurewebsites.net/api/Customers");//List URL
+                CustomerList = JsonConvert.DeserializeObject<List<Customer>>(getResult);// get List
+            }
+            catch (Exception)
+            {
+              
+            }
+
+            return CustomerList;
+        }
+
+        public Customer GetCustomersById(int custId)
+        {
+            Customer customerObj = new Customer();
+            try
+            {
+                 var getResult = consumeAPI_GET("https://getinvoices.azurewebsites.net/api/Customer/"+ custId);
+                 customerObj = JsonConvert.DeserializeObject<Customer>(getResult);//Single By Id
+            }
+            catch (Exception)
+            {
+                throw;
+            }            
+            return customerObj;
+        }
+
+        [HttpPost]
+        public string SaveCustomer([FromBody] Customer customerObj)
+        {
+            try
+            {
+                var addData = new Customer();
+                addData.id = customerObj.id;
+                addData.firstname = customerObj.firstname;
+                addData.lastname = customerObj.lastname;
+                addData.email = customerObj.email;
+                addData.phone_Number = customerObj.phone_Number;
+                addData.country_code = customerObj.country_code;
+                addData.gender = customerObj.gender;
+                addData.balance = customerObj.balance;
+
+                var DATA = JsonConvert.SerializeObject(addData);
+                var URL = "https://getinvoices.azurewebsites.net/api/Customer";
+                var getResult = consumeAPI_Post(URL, DATA);
+                return "successfully added";
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public string UpdateCustomers([FromBody] Customer customerObj)
+        {            
+            try
+            {
+                var DATA = JsonConvert.SerializeObject(customerObj);
+                var URL = "https://getinvoices.azurewebsites.net/api/Customer" + customerObj.id;
+                var getResult = consumeAPI_Post(URL, DATA);
+
+                
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return "successfully updated";
+        }
+
+        [HttpDelete]
+        public string DeleteCustomersById(int custId)
+        {
+            try
+            {
+                var getResult = consumeAPI_DELETE("https://getinvoices.azurewebsites.net/api/Customer/" + custId);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return "data deleted successfully";
+
+        }
+
+
+        public static string consumeAPI_Post(string URL, string DATA)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+            request.ContentLength = DATA.Length;
+            using (Stream webStream = request.GetRequestStream())
+            using (StreamWriter requestWriter = new StreamWriter(webStream, System.Text.Encoding.ASCII))
+            {
+                requestWriter.Write(DATA);
+            }
+            try
+            {
+                HttpWebResponse webResponse = (HttpWebResponse)request.GetResponse();
+
+                using (Stream webStream = webResponse.GetResponseStream() ?? Stream.Null)
+                using (StreamReader responseReader = new StreamReader(webStream))
+                {
+                    string response = responseReader.ReadToEnd();
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            return "No response";
+        }
+
+        public static string consumeAPI_GET(string URL)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            try
+            {
+                WebResponse webResponse = request.GetResponse();
+                using (Stream webStream = webResponse.GetResponseStream() ?? Stream.Null)
+                using (StreamReader responseReader = new StreamReader(webStream))
+                {
+                    string response = responseReader.ReadToEnd();
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            return "No response";
+        }
+
+        public static string consumeAPI_DELETE(string URL)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "DELETE";
+            request.ContentType = "application/json";
+            try
+            {
+                WebResponse webResponse = request.GetResponse();
+                using (Stream webStream = webResponse.GetResponseStream() ?? Stream.Null)
+                using (StreamReader responseReader = new StreamReader(webStream))
+                {
+                    string response = responseReader.ReadToEnd();
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            return "No response";
+        }
+    }
+}
---
+++ CustomerWebApp/ScriptsNg/Controller/CustomerCtrl.js
@@ -0,0 +1,104 @@
+﻿app.controller('CustomerCtrl', ['$scope', 'CrudService',
+    function ($scope, CrudService) {
+        // Base Url   
+        var baseUrl = '/api/Customers/';
+        $scope.btnText = "Save";
+        $scope.customerID = 0;
+        $scope.SaveUpdate = function () {
+            var customer = {
+                FirstName: $scope.firstName,
+                LastName: $scope.lasttName,
+                Email: $scope.email,
+                Phone_Number: $scope.phoneNumber,
+                Country_code: $scope.countrycode,
+                Gender: $scope.gender,
+                Balance: $scope.balance
+            }
+            if ($scope.btnText == "Save") {
+                var apiRoute = baseUrl + 'SaveCustomer/';
+                var saveCustomer = CrudService.post(apiRoute, customer);
+                saveCustomer.then(function (response) {
+                    if (response.data != "") {
+                        alert("Data Save Successfully");
+                        $scope.Clear();
+                    } else {
+                        alert("Some error");
+                    }
+                }, function (error) {
+                    console.log("Error: " + error);
+                });
+            }
+            else {
+                var apiRoute = baseUrl + 'UpdateCustomers/';
+                var UpdateCustomer = CrudService.put(apiRoute, customer);
+                UpdateCustomer.then(function (response) {
+                    if (response.data != "") {
+                        alert("Data Update Successfully");
+                        $scope.GetCustomers();
+                        $scope.Clear();
+                    } else {
+                        alert("Some error");
+                    }
+                }, function (error) {
+                    console.log("Error: " + error);
+                });
+            }
+        }
+        $scope.Clear = function () {
+            $scope.customerID = 0;
+            $scope.firstName = "";
+            $scope.lasttName = "";
+            $scope.email = "";
+            $scope.phoneNumber = "",
+            $scope.countrycode = "";
+            $scope.gender = "";
+            $scope.balance = "";
+        }
+        $scope.GetCustomers = function () {
+            var apiRoute = baseUrl + 'GetCustomers/';
+            var customer = CrudService.getAll(apiRoute);
+            customer.then(function (response) {
+                debugger
+                $scope.customers = response.data;
+            }, function (error) {
+                console.log("Error: " + error);
+            });
+        }
+        $scope.GetCustomers();
+
+        $scope.GetCustomerByID = function (dataModel) {
+            debugger
+            var apiRoute = baseUrl + 'GetCustomersById';
+            var customer = CrudService.getbyID(apiRoute, dataModel.CustomerID);
+            customer.then(function (response) {
+                $scope.customerID = response.data.CustomerID;
+                $scope.firstName = response.data.FirstName;
+                $scope.lasttName = response.data.LastName;
+                $scope.email = response.data.Email;
+                $scope.phoneNumber = response.data.Phone_Number,
+                    $scope.countrycode = response.data.Country_code;
+                $scope.gender = response.data.Gender;
+                $scope.balance = response.data.Balance;
+                $scope.btnText = "Update";
+            }, function (error) {
+                console.log("Error: " + error);
+            });
+        }
+        $scope.DeleteCustomer = function (dataModel) {
+            debugger
+            var apiRoute = baseUrl + 'DeleteCustomersById/' + dataModel.CustomerID;
+            var deleteCustomer = CrudService.delete(apiRoute);
+            deleteCustomer.then(function (response) {
+                if (response.data != "") {
+                    alert("Data Delete Successfully");
+                    $scope.GetCustomers();
+                    $scope.Clear();
+                } else {
+                    alert("Some error");
+                }
+            }, function (error) {
+                console.log("Error: " + error);
+            });
+        }
+    }
+]);
